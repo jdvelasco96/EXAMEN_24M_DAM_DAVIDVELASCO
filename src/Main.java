ea3316a6c0ed53a44c9fe80152f3b435278b5c11
Ceinmark <Ceinmark@DESKTOP-AURVFUI>
2021-03-24 11:43:15 +0100

    Numero Primo

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,28 @@ public class Main {
 	public static void main(String[] args) {
 		
 		Scanner esc = new Scanner (System.in);
-		int numero = 0;
+		float numero = 0;
 		System.out.println("Buenos dias Ceinmark, soy Juan David Velasco" + "\n"
 				+ "Introduce un número para averiguar si es primo o no:\n" );
 		
-		numero = esc.nextInt();
+		numero =(float)esc.nextInt();
+		int contador = 0;
+		String div= "";
 		
+		for (int i = 1; i < numero; i++) {
+			
+			if ((numero/i) == 1) {
+				contador++;
+				div += i + " ";
+			}
+	
+		}
+		if (contador > 1) {
+			System.out.println("el numero no es primo");
+			System.out.println("sus divisores son "+ div);
+		}else {
+			System.out.println("el numero es primo");
+		}
 		
 	}
 
