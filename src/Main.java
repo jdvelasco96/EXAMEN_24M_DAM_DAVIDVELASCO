7e22b1e6f69c9bd17ae55837f8799e05fc190923
Ceinmark <Ceinmark@DESKTOP-AURVFUI>
2021-03-24 11:20:39 +0100

    Primero

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,17 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-
+		Scanner esc = new Scanner (System.in);
+		int numero = 0;
+		System.out.println("Buenos dias Ceinmark, soy Juan David Velasco" + "\n"
+				+ "Introduce un número para averiguar si es primo o no:\n" );
+		
+		numero = esc.nextInt();
+		
+		
 	}
 
 }
